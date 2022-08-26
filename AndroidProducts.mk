727af424b1e26777de6b33929d5db7c01d24de04
Sri19944 <ansridhar1994@gmail.com>
2022-08-26 18:48:44 +0530

    Evolution

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/evolution_Mi439.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_Mi439-user \
+    evolution_Mi439-userdebug \
+    evolution_Mi439-eng
